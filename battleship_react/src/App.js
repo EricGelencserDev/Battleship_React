0dd4faf51f4aaa802cc34be258a478f2f8210ee3
Jake Parolin <jakeparolin@Jakes-MacBook-Pro.local>
2018-04-25 16:50:48 -0700

    Added battleship project

--- battleship_react/src/App.js
+++ battleship_react/src/App.js
@@ -1,21 +1,31 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Board from './components/board.js'
+import "./App.css"
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
+    handleClick() {
+        window.location.reload()
+    }
+
+    render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div>
+                <Route exact path ="/" render={ props => (
+                    <div>
+                        <h1> Battleship </h1>
+
+                        <Board />
+                        <Button id="resetButton" onClick={this.handleClick}>New game</Button>
+                    </div>
+                )} />
+            </div>
+        </Router>
     );
-  }
+    }
 }
 
 export default App;
